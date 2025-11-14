9c9f660b2f4e65156463aceda69727e53def387e
General090 <gospelkalu33@gmail.com>
2025-11-15 00:53:01 +0100

    updated

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import Woman from "../../assets/woman.png";
+import Woman2 from "../../assets/woman2.png";
+import Speak from "../../assets/speaks.png";
+import Voice from "./Voice";
+
+const Home = () => {
+  return (
+    <div className="bg-[#dcd3ca]">
+      <div className="min-h-screen relative overflow-hidden pb-50">
+        <nav className="absolute top-10 right-6 flex gap-5 mb-10 text-[12px] text-[#d84136]">
+          <a href="#">HOME</a>
+          <a href="#">ABOUT</a>
+          <a href="#">VOICE</a>
+          <a href="#">BOOK</a>
+          <a href="#">SHOP</a>
+          <a href="#">CONTACT</a>
+        </nav>
+
+        <div className="grid grid-cols-1 md:grid-cols-2">
+          <div className="relative">
+            <img src={Speak} className="absolute top-10 w-[25%] left-10" />
+
+            <img
+              src={Woman}
+              className="w-[90%] h-[100vh] object-cover"
+              alt=""
+            />
+
+            <h1 className="absolute top-[40%] left-100 font-[Bebas Neue] text-[#d84136] text-8xl">
+              IGNITING
+              <br />
+              <span className="inline-block ml-25">PURPOSE</span>
+            </h1>
+          </div>
+
+          <div>
+            <h1 className="absolute top-25 left-[50rem] font-[Bebas Neue] text-lg">
+              GOSPEL-CENTERED MOVEMENT
+              <br />
+              <span className="inline-block ml-25">COMMUNITY+MINITRY</span>
+            </h1>
+
+            <div className="flex items-center justify-center h-screen">
+              <img
+                src={Woman2}
+                className="w-[50%] h-[500px] object-cover mt-60 rounded-xl"
+              />
+            </div>
+          </div>
+        </div>
+
+        <h1 className="transform rotate-90 absolute top-[40rem] right-[30rem] font-[Bebas Neue] text-xl text-[#2e2c2b]">
+          WITH AUTHOR, SPEAKER AND
+          <br />
+          <span className="inline-block ml-25">COACH MARIELA ROSARIO</span>
+        </h1>
+      </div>
+
+      <Voice />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/Pages/Home/Voice.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Voice = () => {
+  return (
+    <div className='pt-20 flex flex-col justify-center items-center mx-auto w-[60%] text-[#2e2c2b]'>
+      <h1 className='text-8xl text-center'>FIND YOUR VOICE & ACTIVATE YOUR PURPOSE</h1>
+      <h2 className='py-10 text-2xl'>COACHING FOR THE SOUL</h2>
+      <p className='text-center'>If you’ve been feeling the tension of being called by God, but held back by fear, shame, or the weight of everyone else’s expectations, you’re not alone and you don’t have to stay stuck. This is soul work. We go deep, invite the Holy Spirit in, and uncover the fire God placed inside you.</p>
+      <p className='py-10'>It's time to get your voice back.</p>
+
+      <button className='mb-10 bg-[#d84136] py-3 px-10 rounded-xl text-white'>WORK WITH ME</button>
+    </div>
+  )
+}
+
+export default Voice
